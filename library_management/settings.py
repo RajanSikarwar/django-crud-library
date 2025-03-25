f28ff4ff56491937b11e222bd0a4d720f531d04d
Rajan <rajansikarwar.ravindra@gmail.com>
2025-03-25 17:31:46 +0530

    Implementing library management using crud apis

--- library_management/settings.py
+++ library_management/settings.py
@@ -20,12 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-bgz@ww2s$ha+ybj3tm44ux%$-c4js8j6f8_t%i+jb3zo=&d6cw"
 
